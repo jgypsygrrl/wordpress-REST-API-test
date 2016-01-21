b2e4e989c27405d30a81db622536f899a6f20e98
Jennifer <jennifergapay@gmail.com>
2016-01-21 00:04:28 -0500

    wpRESTapi: Add baseline HTML & functionality to plugin

--- wp-content/plugins/moreposts/moreposts.php
+++ wp-content/plugins/moreposts/moreposts.php
@@ -21,3 +21,32 @@ function moreposts_scripts() {
 
 }
 add_action( 'wp_enqueue_scripts', 'moreposts_scripts' );
+
+
+// base HTML to be added to the bottom of a post
+function moreposts_baseline_html() {
+  //container
+  $baseline  = '<section id="related-posts" class="related-posts">';
+  $baseline .= '<a href="#" class="get-related-posts">Get related posts</a>';
+  $baseline .= '<div class="ajax-loader"><img src="' . plugin_dir_url( __FILE__ ) . 'css/spinner.svg" width="32" height="32" /></div>';
+  $baseline .= '</section><!-- .related-posts -->';
+
+  return $baseline;
+}
+
+
+// adds above to the bottom of single posts.  this function runs the entire plugin
+function moreposts_display($content) {
+  if( is_single() && is_main_query() ) {
+    //grabs the post content, append baseline to the content & return content using a filter
+    $content .= moreposts_baseline_html();
+  }
+    return $content;
+}
+add_filter( 'the_content', 'moreposts_display');
+
+
+
+
+
+
